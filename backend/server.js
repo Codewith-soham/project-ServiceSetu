8cd638cdadb5eaea7b4e9514d790d613ea7f5df5
ghadgesoham <ghadgesoham093@gmail.com>
2026-02-15 17:09:27 +0530

    Integrated MongoDb database connection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import dotenv from 'dotenv';
 import { app } from './src/app.js';
+import connectDB from './src/db/connection.js';
 
 dotenv.config({             //setup dotenv to load environment variables from .env file
-    path: "./env"
+    path: "./.env"
 });
 
 const port = process.env.PORT || 8000;   //get the port from environment variable or use default 8000
 
-
-app.listen(port, (req,res) => {
-    console.log(`Server running on port ${port}`);
-    
-})
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    })
+    .catch(() => {
+        console.error("Failed to connect to the database", err);
+    })
