f37aac4c5d397975ca4d54a15dc9961b16848236
ServiceSetu Developer <dev@servicesetu.com>
2026-02-22 20:31:43 +0530

    Created bookingSchema model

---
+++ backend/src/models/booking.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+    user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ServiceProvider",
+    required: true
+  },
+
+  serviceType: {
+    type: String,
+    enum: ["care taker", "maid", "electrician", "plumber"],
+    required: true
+  },
+
+  bookingDate: {
+    type: Date,
+    required: true
+  },
+
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending"
+  }
+}, { timestamps: true }); 
+
+export const Booking = mongoose.model("Booking", bookingSchema);
