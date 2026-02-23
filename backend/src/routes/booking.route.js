aed9e7497c38d7c8dc658fcba5b6430ae24ba09b
ServiceSetu Developer <dev@servicesetu.com>
2026-02-23 22:02:36 +0530

    Added cancel booking getuserbooking and getproviderbooking routes

--- backend/src/routes/booking.route.js
+++ backend/src/routes/booking.route.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createBooking, bookingStatus } from "../controllers/booking.controller.js";
+import { createBooking, bookingStatus , getUserBooking, getProviderBookings, cancelBooking } from "../controllers/booking.controller.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.route('/create').post(verifyToken, createBooking)
-router.patch("/:bookingId/status", bookingStatus); //patch -- update booking status by provider (accept/reject) or by user (cancel)
+router.patch("/:bookingId/status", verifyToken, bookingStatus); //patch -- update booking status by provider (accept/reject) or by user (cancel)
+router.get("/user-bookings", verifyToken, getUserBooking) //get all bookings of a user (both provider and user can see their bookings)
+router.get("/provider-bookings", verifyToken, getProviderBookings) //get all bookings of a provider (only provider can see their bookings)
+router.patch("/:bookingId/cancel", verifyToken, cancelBooking) //cancel booking by user (only user can cancel their booking)
 
 export default router;
